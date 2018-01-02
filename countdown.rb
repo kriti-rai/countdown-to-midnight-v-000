f34116adee0832c1b1773334c9ba6102e5643582
Kriti Rai <kriti.rai@gmail.com>
2018-01-02 01:19:18 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -6,13 +6,12 @@ def countdown
   end
   return "HAPPY NEW YEAR!"
  end
-countdown
-# def countdown_with_sleep
-#   number = 10
-#   while number > 0
-#     puts "#{number} SECOND(S)!"
-#     sleep(1)
-#     number -=1
-#   end
-#   puts "HAPPY NEW YEAR!"
-# end
+def countdown_with_sleep
+  number = 10
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+    number -=1
+  end
+  return "HAPPY NEW YEAR!"
+end
